6390b41d03e13d15ac5958b6c5f5a112e1058d69
a.rashkov <a.rashkov@astoundcommerce.com>
2023-03-09 10:10:30 +0200

    Handling Http States & Feedback with Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { Fragment } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
 
   const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  const dispatch = useDispatch();
+
   /*
   * Send data to server
   * ASYNC CALLS SHOULD NOT BE DONE INSIDE REDUX
@@ -15,17 +22,48 @@ function App() {
   * once updated, the redux state data is send to server
   */
   useEffect(() => {
-    fetch('https://udemy-redux-advanced-db615-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
-      method: 'PUT', // PUT request should overwrite existing server data
-      body: JSON.stringify(cart)
-    })
-  }, [cart]);
+
+    const sendData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Pending',
+        message: 'Sending data....'
+      }));
+
+      const response = await fetch('https://udemy-redux-advanced-db615-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+        method: 'PUT', // PUT request should overwrite existing server data
+        body: JSON.stringify(cart)
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success',
+        message: 'Data has been sent to server'
+      }));
+    };
+
+
+    sendData().catch(err => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: err.message
+      }));
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
